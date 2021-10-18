0321b72fd740bda65743f1f4803733045edff342
Prithivi <prithivi8teen@gmail.com>
2021-10-18 17:48:51 +0530

    final commit

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+import {Fade,Stagger} from 'react-animation-components';
+
 
 function About(props) {
-
-    const leaders = props.leaders.map((leader) => {
+    if(props.isLoading){
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if(props.errMess){
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
-            <p>
-              <RenderLeader leader ={leader}/>
-            </p>
+            <Stagger in>
+                <Fade in>
+                    <li key={leader.id}>
+                        <RenderLeader leader={leader} />
+                    </li>
+                </Fade>
+            </Stagger>
         );
     });
 
@@ -17,7 +42,7 @@ function About(props) {
           <div className="col-12">
         <Media>
           <Media left>
-            <Media object src={leader.image} alt={leader.name} key={leader.id}/>
+            <Media object src={baseUrl+leader.image} alt={leader.name} key={leader.id}/>
           </Media>
           
             <Media body right>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -7,7 +7,7 @@ export const Comments=(state={
     switch(action.type){
         case ActionTypes.ADD_COMMENTS:
             return {...state,isLoading:false,errMess:null,comments:action.payload};
-            case ActionTypes.COMMENTS_FAILED:
+        case ActionTypes.COMMENTS_FAILED:
                 return {...state,isLoading:false,errMess:action.payload,comments:[]};
         case ActionTypes.ADD_COMMENT:
             var comment=action.payload;
